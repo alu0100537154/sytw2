93cbb7a7970f95c26949869f4fd2be950d09255f
Debora MPB <deborampb@gmail.com>
2014-09-24 15:06:35 +0100

    Primer commit

---
+++ twitter.rb
@@ -0,0 +1,28 @@
+
+require 'twitter'
+require 'sinatra'
+
+require './configure'
+
+get '/' do
+  @nfriends = []
+  @name = ''
+  @number = 0
+  erb :twitter
+end
+
+post '/' do
+  @nfriends = []
+  client = my_twitter_client() 
+  @name = params[:firstname] || ''
+  @number = params[:n].to_i
+  @number = 1 if @number < 1
+  if (client.user? @name) && (@number <= 10)
+ ultimos_t = client.friends(@name,{:skip_status => 1, :include_user_entities => false})
+    @nfriends =(@nfriends != '') ? ultimos_t.map{|i| [i.name ,i.followers_count]}: ''
+    @nfriends.sort_by!{|c,d| d}.reverse!
+    
+  end
+  erb :twitter
+end
+
